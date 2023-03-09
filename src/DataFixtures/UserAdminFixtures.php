778f490052c880ff3b0c505c784698c7204c2c34
zero <teclaizerai@gmail.com>
2023-03-09 13:41:14 +0100

    Agg. dev data fixture per admin user.

---
+++ src/DataFixtures/UserAdminFixtures.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserAdminFixtures extends Fixture implements FixtureGroupInterface
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setEmail('admin@example.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword(
+            $this->passwordHasher->hashPassword($admin, 'admin')
+        );
+
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['dev', 'devSecurity'];
+    }
+}
