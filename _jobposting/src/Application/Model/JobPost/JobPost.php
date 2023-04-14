8dc9bf223522a6a8cc4be902234959007cfa3c2e
zero <teclaizerai@gmail.com>
2023-04-15 01:07:58 +0200

    Aggiornamento modello Jobpost (publicationStart|publicationEnd).

--- _jobposting/src/Application/Model/JobPost/JobPost.php
+++ _jobposting/src/Application/Model/JobPost/JobPost.php
@@ -12,6 +12,10 @@ class JobPost
 
     private string $description;
 
+    private ?\DateTimeImmutable $publicationStart = null;
+
+    private ?\DateTimeImmutable $publicationEnd = null;
+
     public function __construct(UuidInterface $id, string $title)
     {
         $this->id = $id;
@@ -46,5 +50,25 @@ class JobPost
     public function setDescription(string $description): void
     {
         $this->description = $description;
+    }
+
+    public function getPublicationStart(): ?\DateTimeImmutable
+    {
+        return $this->publicationStart;
+    }
+
+    public function setPublicationStart(\DateTimeImmutable $publicationStart): void
+    {
+        $this->publicationStart = $publicationStart;
+    }
+
+    public function getPublicationEnd(): ?\DateTimeImmutable
+    {
+        return $this->publicationEnd;
+    }
+
+    public function setPublicationEnd(\DateTimeImmutable $publicationEnd): void
+    {
+        $this->publicationEnd = $publicationEnd;
     }
 }
